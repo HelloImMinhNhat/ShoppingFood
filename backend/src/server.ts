a7ef049c88c8601af170b14c9f17caa1522a88db
HelloImMinhNhat <MinhNhat.1201.dl@gmail.com>
2023-11-30 18:32:01 +0700

    update server cors header

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,6 +29,13 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'index.html'))
 })
 const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.WEB_URL);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
 })
