58a5a203f6f3abd5cc2bbcb551d5c762f549acd0
HelloImMinhNhat <MinhNhat.1201.dl@gmail.com>
2023-11-30 18:34:18 +0700

    update server cors header

--- backend/built/server.js
+++ backend/built/server.js
@@ -31,6 +31,12 @@ app.get('*', function (req, res) {
     res.sendFile(path_1.default.join(__dirname, 'public', 'index.html'));
 });
 var port = process.env.PORT || 5000;
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', process.env.WEB_URL);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.listen(port, function () {
     console.log("Website served on http://localhost:" + port);
 });
