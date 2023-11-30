160991ba0bfe9ffd6dee7d51bafc19057e10ad9e
HelloImMinhNhat <MinhNhat.1201.dl@gmail.com>
2023-11-30 20:20:44 +0700

    update server cors

--- backend/built/share/urls.js
+++ backend/built/share/urls.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WHITELIST_DOMAINS = void 0;
 exports.WHITELIST_DOMAINS = [
-    'http://localhost:4200/',
-    'https://shopping-food.onrender.com/'
+    'http://localhost:4200',
+    'https://shopping-food.onrender.com'
 ];
