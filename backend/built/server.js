2f67ffd683a5dff257de0d6596d3768bc73f917b
HelloImMinhNhat <MinhNhat.1201.dl@gmail.com>
2023-11-30 19:13:15 +0700

    update server cors header

--- backend/built/server.js
+++ backend/built/server.js
@@ -17,26 +17,34 @@ var coupon_router_1 = __importDefault(require("./routers/coupon.router"));
 (0, database_config_1.dbConnect)();
 var app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({
+var options = {
+    allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+    ],
     credentials: true,
-    origin: ['http://localhost:4200']
-}));
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: "http://localhost:4200",
+    preflightContinue: false,
+};
+app.use((0, cors_1.default)(options));
+// app.use(cors({
+//     credentials: true,
+//     origin: ['http://localhost:4200']
+// }));
 app.use("/api/foods", food_router_1.default);
 app.use("/api/coupons", coupon_router_1.default);
 app.use("/api/users", user_router_1.default);
 app.use("/api/orders", order_router_1.default);
 app.use("/api/categories", category_router_1.default);
-app.use(express_1.default.static('public'));
-app.get('*', function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, 'public', 'index.html'));
+app.use(express_1.default.static("public"));
+app.get("*", function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, "public", "index.html"));
 });
 var port = process.env.PORT || 5000;
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', process.env.WEB_URL);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 app.listen(port, function () {
     console.log("Website served on http://localhost:" + port);
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,50 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import path, { dirname } from 'path';
+import path, { dirname } from "path";
 import express from "express";
 import cors from "cors";
-import foodRouter from './routers/food.router'
-import userRouter from './routers/user.router'
-import { dbConnect } from './configs/database.config';
-import orderRouter from './routers/order.router';
-import categoryRouter from './routers/category.router';
-import couponRouter from './routers/coupon.router';
+import foodRouter from "./routers/food.router";
+import userRouter from "./routers/user.router";
+import { dbConnect } from "./configs/database.config";
+import orderRouter from "./routers/order.router";
+import categoryRouter from "./routers/category.router";
+import couponRouter from "./routers/coupon.router";
 dbConnect();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:4200']
-}));
+
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "X-Access-Token",
+  ],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: "http://localhost:4200",
+  preflightContinue: false,
+};
+app.use(cors(options));
+
+// app.use(cors({
+//     credentials: true,
+//     origin: ['http://localhost:4200']
+// }));
 
 app.use("/api/foods", foodRouter);
-app.use("/api/coupons", couponRouter)
+app.use("/api/coupons", couponRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/categories", categoryRouter);
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'index.html'))
-})
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 const port = process.env.PORT || 5000;
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.WEB_URL);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
 app.listen(port, () => {
-    console.log("Website served on http://localhost:" + port);
-})
+  console.log("Website served on http://localhost:" + port);
+});
