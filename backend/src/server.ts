b150653ea3b2a3defc71001f2765e6a6172fb48c
HelloImMinhNhat <MinhNhat.1201.dl@gmail.com>
2023-12-08 14:36:57 +0700

    update auth.mid.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
+import fs from 'fs';
+import https from 'https';
 import express from "express";
 import cors, { CorsOptions } from "cors";
 import foodRouter from "./routers/food.router";
@@ -31,7 +33,20 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log("Website served on http://localhost:" + port);
+// });
+const privateKey = fs.readFileSync('path/to/private-key.pem', 'utf8');
+const certificate = fs.readFileSync('path/to/certificate.pem', 'utf8');
+const ca = fs.readFileSync('path/to/ca.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate, ca: ca };
+
+const httpsServer = https.createServer(credentials, app);
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log("Website served on http://localhost:" + port);
+
+httpsServer.listen(port, () => {
+  console.log(`Website served on https://shopping-food.onrender.com:${port}`);
 });
